007b34fce77bea4737ff7d691ec899ee300fee87
thilina_dilshan <thilina.dilshan@arimaclanka.com>
2022-05-22 10:08:31 +0530

    change

--- src/http/Http.js
+++ src/http/Http.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 export const httpRequest = async ({url, method, data})=>{
     const requset = await axios({
-        // url : 'https://car-management-app-university.herokuapp.com/' + url,
-         url : 'http://localhost:4000/' + url,
+        url : 'https://car-management-app-university.herokuapp.com/' + url,
+        // url : 'http://localhost:4000/' + url,
         method,
         data : data
     })
